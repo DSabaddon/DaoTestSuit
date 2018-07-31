f74e2cd70d579cc9f5a8ba39e8d3beb307847e33
maksimenko <d.maksimenko@ftc.ru>
2018-07-31 18:43:44 +0700

    Увеличена версия gradle до 4.8 и другие мелкие изменения

--- runner/src/main/java/ru/ftc/pc/testing/dao/TestController.java
+++ runner/src/main/java/ru/ftc/pc/testing/dao/TestController.java
@@ -2,7 +2,7 @@ package ru.ftc.pc.testing.dao;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.ftc.pc.testing.dao.component.DatabaseInitializer;
 import ru.ftc.pc.testing.dao.model.Row;
@@ -20,7 +20,7 @@ import java.util.List;
 public class TestController {
   private final DatabaseInitializer databaseInitializer;
 
-  @PostMapping("/initDb")
+  @GetMapping("/initDb")
   void initDb() throws IOException {
     databaseInitializer.initDb("/create_tables.sql");
 
